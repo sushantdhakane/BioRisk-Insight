b73c0f36d9009ac106fcf80b53b6767cab8c41db
sushantdhakane <168280539+sushantdhakane@users.noreply.github.com>
2024-08-16 19:24:10 +0530

    Delete biodiversity-project/frontend directory

--- biodiversity-project/frontend/config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { override, addBabelPlugins } = require('customize-cra');
-
-module.exports = override(
-  ...addBabelPlugins(
-    ["@babel/plugin-proposal-class-properties", { "loose": true }],
-    ["@babel/plugin-proposal-private-methods", { "loose": true }],
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
-  )
-);
