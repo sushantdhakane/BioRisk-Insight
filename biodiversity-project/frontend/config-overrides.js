2d976a387ff8234481133219e69583d13a4bc0c0
sushantdhakane <168280539+sushantdhakane@users.noreply.github.com>
2024-08-17 19:20:21 +0530

    Update config-overrides.js

--- biodiversity-project/frontend/config-overrides.js
+++ biodiversity-project/frontend/config-overrides.js
@@ -1,7 +1,7 @@
 const { override, addBabelPreset } = require('customize-cra');
 
 
-module.exports override(
+module.exports = override(
   addBabelPreset('@babel/preset-env'),
   addBabelPreset('@babel/preset-react')
 );
